dbc8b455569f1bb245b9eb74adf0d91318340418
vinhho3010 <anhvinh30102001@gmail.com>
2023-09-24 11:20:37 +0700

    chore: load class detail

--- src/app/pages/ministry/class-list/class-detail/class-detail.component.ts
+++ src/app/pages/ministry/class-list/class-detail/class-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ClassService } from 'src/app/services/class.service';
 import { ToastService } from 'src/app/services/local/toast.service';
+import { MajorService } from 'src/app/services/major.service';
 
 @Component({
   selector: 'app-class-detail',
@@ -10,19 +12,60 @@ import { ToastService } from 'src/app/services/local/toast.service';
 })
 export class ClassDetailComponent {
   classId = '';
+  classDetailForm: FormGroup;
+  majors: any[] = [];
 
-    constructor(private activatedRoute: ActivatedRoute, private classService: ClassService, private toastService: ToastService) { }
+    constructor(private activatedRoute: ActivatedRoute, private classService: ClassService, private toastService: ToastService, private majorService: MajorService) {
+      this.classDetailForm = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        semester: new FormControl('', [Validators.required]),
+        schoolYear: new FormControl('', [Validators.required]),
+        teacher: new FormControl('', [Validators.required]),
+        major: new FormControl('', [Validators.required]),
+        studentCount: new FormControl('', [Validators.required]),
+
+      });
+    }
 
     ngOnInit(): void {
       this. classId = this.activatedRoute.snapshot.params['id'];
+      this.loadMajorList();
       this.loadClassInfo();
+    }
+
+    loadMajorList(): void {
+      this.majorService.getAllmajor().subscribe({
+        next: (res) => {
+          this.majors = res;
+        },
+        error: (err) => {
+          this.toastService.showErrorToast(err.error.message);
+        }
+      })
+    }
+
+    initDetailData(data: any): void {
+      this.classDetailForm.setValue({
+        name: data.name,
+        semester: data.semester,
+        schoolYear: data.schoolYear,
+        teacher: data.teacher.fullName,
+        major: data.major,
+        studentCount: data.student.length,
+      })
     }
 
     loadClassInfo(): void {
       this.classService.getClassInfo(this.classId).subscribe({
         next: (res) => {
-          console.log(res);
-
+          this.classDetailForm.setValue({
+            name: res.name,
+            semester: res.semester,
+            schoolYear: res.schoolYear,
+            teacher: res.teacher.fullName,
+            major: res.major,
+            studentCount: res.student.length,
+          });
         },
         error: (err) => {
           this.toastService.showErrorToast(err.error.message);
